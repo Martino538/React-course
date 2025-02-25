3ce151cc93896962accb0e8fb98bdb7253f81fe5
Stefan Janssen <stefanjanssen@MacBook-Pro-van-Stefan.local>
2025-02-25 11:19:13 +0100

    added two-way-binding

---
+++ tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnTitle = "Edit";
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleNameChange}></input>
+    );
+    btnTitle = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{btnTitle}</button>
+    </li>
+  );
+}
