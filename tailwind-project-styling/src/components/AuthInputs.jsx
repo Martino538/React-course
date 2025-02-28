a97363ec0a342c29365628f1d841d5f8e22fc8b9
Martijn Kooijman <martijnkooijman12@gmail.com>
2025-02-28 17:33:17 +0100

    more tailwindcss

--- tailwind-project-styling/src/components/AuthInputs.jsx
+++ tailwind-project-styling/src/components/AuthInputs.jsx
@@ -3,13 +3,6 @@ import { styled } from 'styled-components';
 import Button from './Button.jsx'
 import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -31,13 +24,13 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className='flex flex-col gap-2 mb-6'>
           <Input
             label="Email"
             type="email"
             onChange={(event) => handleInputChange('email', event.target.value)}
-            $invalid={emailNotValid}
+            invalid={emailNotValid}
           />
           <Input
             label="Password"
@@ -45,11 +38,11 @@ export default function AuthInputs() {
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
-            $invalid={passwordNotValid}
+            invalid={passwordNotValid}
           />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
