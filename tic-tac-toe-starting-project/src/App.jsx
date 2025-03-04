c11d8698fd8fb9029a80f7b960b9738420e56c40
Stefan Janssen <stefanjanssen@MacBook-Pro-van-Stefan.local>
2025-03-04 14:09:30 +0100

    added game logic and button disabling

--- tic-tac-toe-starting-project/src/App.jsx
+++ tic-tac-toe-starting-project/src/App.jsx
@@ -1,12 +1,28 @@
-import Player from "./components/Player/Player"
-import GameBoard from "./components/GameBoard/Gameboard"
 import { useState } from "react"
 
+import Player from "./components/Player/Player"
+import GameBoard from "./components/GameBoard/Gameboard"
+import Log from "./components/Log/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: activePlayer },
+         ...prevTurns]
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -16,8 +32,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
